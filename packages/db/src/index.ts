19d786c8ce68a765c1a14570acb13ee37432adf0
Tamás Vince Kornél <vince@tvk.lol>
2025-03-17 12:07:12 +0100

    fix(db): export additional client types and update exports in index.ts

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,2 +1,7 @@
 export { prisma } from './client';
+export * from '../generated/client/runtime/library';
 export * from '../generated/client';
+
+export type { 
+  PrismaPromise
+} from '../generated/client';
