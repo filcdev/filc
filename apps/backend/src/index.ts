acff8680694154b423be7c7f61019bfdf420a9a1
Tamás Vince Kornél <vince@tvk.lol>
2025-03-18 11:58:19 +0100

    feat: add CORS support to the backend and update dependencies

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,13 @@
 import * as trpcExpress from '@trpc/server/adapters/express'
 import express from 'express'
+import type { Request } from "express";
+import cors from "cors";
 
 import { appRouter, createTRPCContext } from '@filc/api'
 
 const app = express()
+
+app.use(cors<Request>());
 
 app.use(
   '/trpc',
@@ -16,8 +20,11 @@ app.use(
   })
 )
 
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
 app.listen(4000)
+
+console.log('Server is running on http://localhost:4000')
