bc772b80641697d19a97a825feacb52d8d11d839
Tamás Vince Kornél <vince@tvk.lol>
2025-03-21 16:35:35 +0100

    feat(auth): add error handling for login and registration processes

--- apps/frontend/src/components/Auth.tsx
+++ apps/frontend/src/components/Auth.tsx
@@ -3,6 +3,7 @@ import { useAuth } from '@/utils/auth'
 
 import ClassSelector from './ClassSelector'
 import TextInput from './TextInput'
+import { TRPCClientError } from '@trpc/client'
 
 type AuthState = 'login' | 'register'
 
@@ -12,19 +13,26 @@ const Auth = () => {
   const [email, setEmail] = useState<string>('')
   const [username, setUserName] = useState<string>('')
   const [classId, setClassId] = useState<string>('')
+  const [error, setError] = useState<string>('')
 
   const [authState, setAuthState] = useState<AuthState>('login')
   const stateText = authState === 'login' ? 'Bejelentkezés' : 'Regisztráció'
 
   const onLogin = async () => {
+    setError('')
     try {
       await login({ email, password })
-    } catch (err) {
-      console.error(err)
+    } catch (err: unknown) {
+      if (err instanceof TRPCClientError) {
+        setError(err.message)
+      } else {
+        console.error(err)
+      }
     }
   }
 
   const onRegister = async () => {
+    setError('')
     try {
       await register({
         email,
@@ -32,14 +40,23 @@ const Auth = () => {
         username,
         classId
       })
-    } catch (err) {
-      console.error(err)
+    } catch (err: unknown) {
+      if (err instanceof TRPCClientError) {
+        setError(err.message)
+      } else {
+        console.error(err)
+      }
     }
   }
 
   return (
     <div className="mx-auto flex h-screen w-[30%] flex-col items-center justify-center gap-5 self-center text-2xl">
       <h2 className="mb-3 text-6xl">{stateText}</h2>
+      {error && (
+        <div className="mb-3 w-full rounded-md bg-red-100 p-4 text-red-700">
+          {error}
+        </div>
+      )}
       {authState === 'register' && (
         <>
           <ClassSelector onChange={setClassId} />
