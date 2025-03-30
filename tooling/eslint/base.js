24adf792365f6cd1c3fd3cc939097d98b1b58c11
Tamás Vince Kornél <vince@tvk.lol>
2025-03-30 12:12:06 +0200

    fix(eslint): correct path to .gitignore for proper file inclusion

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -9,7 +9,7 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   // Ignore files not tracked by VCS and any config files
-  includeIgnoreFile(path.join(import.meta.dirname, '../../.gitignore')),
+  includeIgnoreFile(path.join(import.meta.dirname, '../../../.gitignore')),
   { ignores: ['**/*.config.*'] },
   {
     files: ['**/*.js', '**/*.ts', '**/*.tsx'],
