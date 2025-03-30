e3f0608b31e2c80d0303eef915c31f307a7b3a67
Tamás Vince Kornél <vince@tvk.lol>
2025-03-30 23:12:46 +0200

    Revert "refactor(expo): downgrade more packages"
    This reverts commit e7616553b4248890d81f164510b7efdaf7fe72a2.

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -41,6 +41,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   plugins: [
     'expo-router',
     'expo-secure-store',
+    'expo-web-browser',
     [
       'expo-splash-screen',
       {
