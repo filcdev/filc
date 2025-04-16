ab07276c948ad28d6d150a316442f48e6e61559d
Tamás Vince Kornél <vince@tvk.lol>
2025-04-16 09:15:17 +0200

    run formatting

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,25 +1,25 @@
-import { useTRPC } from "@/lib/trpc";
-import { useQuery } from "@tanstack/react-query";
-import { createFileRoute } from "@tanstack/react-router";
-import { logo } from "@filc/ui";
+import { useTRPC } from '@/lib/trpc'
+import { logo } from '@filc/ui'
+import { useQuery } from '@tanstack/react-query'
+import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute("/")({
-	component: Index,
-});
+export const Route = createFileRoute('/')({
+  component: Index,
+})
 
 function Index() {
-	const t = useTRPC();
-	const ping = useQuery(t.ping.queryOptions());
+  const t = useTRPC()
+  const ping = useQuery(t.ping.queryOptions())
 
-	if (ping.isLoading) {
-		return <div>Loading...</div>;
-	}
+  if (ping.isLoading) {
+    return <div>Loading...</div>
+  }
 
-	return (
-		<div className="p-2">
-			<img src={logo} alt="" />
-			<h3>Hello from tsr</h3>
-			<h4>{ping.data}</h4>
-		</div>
-	);
+  return (
+    <div className='p-2'>
+      <img src={logo} alt='' />
+      <h3>Hello from tsr</h3>
+      <h4>{ping.data}</h4>
+    </div>
+  )
 }
