8377cabc54f0f33c2450341e1ef59746ec321936
Tamás Vince Kornél <vince@tvk.lol>
2025-04-16 13:51:25 +0200

    add HIBP plugin

--- packages/api/src/auth.ts
+++ packages/api/src/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
-import { admin, organization } from 'better-auth/plugins'
+import { admin, haveIBeenPwned, organization } from 'better-auth/plugins'
 import { config } from './constants'
 import { authSchema, db } from './db'
 import { secondaryStorage } from './db/redis'
@@ -9,7 +9,7 @@ export const auth = betterAuth({
   secret: config.auth.secret,
   baseURL: config.auth.url,
   secondaryStorage,
-  plugin: [admin(), organization()],
+  plugin: [admin(), organization(), haveIBeenPwned()],
   socialProviders: {
     microsoft: {
       clientId: config.entra.clientId,
