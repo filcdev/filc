39fc149bba4093719a57a966087388b42574ca99
Tamás Vince Kornél <vince@tvk.lol>
2025-04-17 19:46:15 +0200

    feat: enhance environment configuration handling in getConfig function

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -3,16 +3,27 @@ import { createProdConfig } from './envs/prod'
 import { createStagingConfig } from './envs/staging'
 
 const getConfig = () => {
-  switch (process.env.APP_ENV) {
+  let env: string | undefined;
+  try {
+    env = process.env.NODE_ENV
+    if (!env) {
+      process && console.warn("NODE_ENV is not set, defaulting to 'development'")
+      env = 'development'
+    }
+  } catch (e) {
+    env = import.meta.env.MODE
+  }
+
+  switch (env) {
     case 'production':
       return createProdConfig()
     case 'staging':
       return createStagingConfig()
-    case 'local':
+    case 'development':
       return createLocalConfig()
     default:
       throw new Error(
-        `Invalid APP_ENV "${process.env.APP_ENV}". If you are running locally, please set APP_ENV=local, like so: "APP_ENV=local bun dev"`
+        `Invalid APP_ENV "${env}". If you are running locally, please set APP_ENV=local, like so: "APP_ENV=local bun dev"`
       )
   }
 }
