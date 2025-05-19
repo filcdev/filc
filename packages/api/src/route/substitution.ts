38ff42d8f7b379d416a911fc8a4027472d770974
Susa Milán Mihály <mailtosnek@protonmail.com>
2025-05-19 11:07:16 +0200

    Update the innputs for the substitution mutations so it fits the new schemas.

--- packages/api/src/route/substitution.ts
+++ packages/api/src/route/substitution.ts
@@ -1,43 +1,43 @@
-import { db } from '@filc/db'
-import { substitution } from '@filc/db/schema/timetable.ts'
-import { TRPCError } from '@trpc/server'
-import { eq } from 'drizzle-orm'
-import { z } from 'zod'
-import { createTRPCRouter, protectedProcedure } from '../trpc'
+import { db } from "@filc/db";
+import { substitution } from "@filc/db/schema/timetable.ts";
+import { TRPCError } from "@trpc/server";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const substitutionRouter = createTRPCRouter({
   create: protectedProcedure
     .input(
       z.object({
-        lesson: z.string(),
-        teacher: z.string(),
-      })
+        lessonId: z.string(),
+        teacherId: z.string(),
+      }),
     )
     .mutation(async ({ input }) => {
       try {
-        await db.insert(substitution).values(input)
+        await db.insert(substitution).values(input);
       } catch (_error) {
         throw new TRPCError({
-          message: 'Failed to create substitution.',
-          code: 'INTERNAL_SERVER_ERROR',
-        })
+          message: "Failed to create substitution.",
+          code: "INTERNAL_SERVER_ERROR",
+        });
       }
     }),
 
   update: protectedProcedure
     .input(
       z.object({
-        teacher: z.string(),
-      })
+        teacherId: z.string(),
+      }),
     )
     .mutation(async ({ input }) => {
       try {
-        await db.update(substitution).set(input)
+        await db.update(substitution).set(input);
       } catch (_error) {
         throw new TRPCError({
-          message: 'Failed to update substitute teacher.',
-          code: 'INTERNAL_SERVER_ERROR',
-        })
+          message: "Failed to update substitute teacher.",
+          code: "INTERNAL_SERVER_ERROR",
+        });
       }
     }),
 
@@ -46,12 +46,12 @@ export const substitutionRouter = createTRPCRouter({
       await db
         .delete(substitution)
         .where(eq(substitution.id, input))
-        .returning()
+        .returning();
     } catch (_error) {
       throw new TRPCError({
-        message: 'Failed to delete substitution.',
-        code: 'INTERNAL_SERVER_ERROR',
-      })
+        message: "Failed to delete substitution.",
+        code: "INTERNAL_SERVER_ERROR",
+      });
     }
   }),
 
@@ -62,15 +62,15 @@ export const substitutionRouter = createTRPCRouter({
         await db
           .select()
           .from(substitution)
-          .where(eq(substitution.lesson, input))
-          .limit(1)
+          .where(eq(substitution.lessonId, input))
+          .limit(1);
 
         // TODO: Maybe join this later?
       } catch (_error) {
         throw new TRPCError({
-          message: 'Failed to get substitution for lesson.',
-          code: 'INTERNAL_SERVER_ERROR',
-        })
+          message: "Failed to get substitution for lesson.",
+          code: "INTERNAL_SERVER_ERROR",
+        });
       }
     }),
 
@@ -81,14 +81,14 @@ export const substitutionRouter = createTRPCRouter({
         await db
           .select()
           .from(substitution)
-          .where(eq(substitution.teacher, input))
+          .where(eq(substitution.teacherId, input));
 
         // TODO: Maybe join this later?
       } catch (_error) {
         throw new TRPCError({
-          message: 'Failed to get substitutions for teacher.',
-          code: 'INTERNAL_SERVER_ERROR',
-        })
+          message: "Failed to get substitutions for teacher.",
+          code: "INTERNAL_SERVER_ERROR",
+        });
       }
     }),
-})
+});
