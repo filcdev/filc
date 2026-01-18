c2478faf860ed08da5d0b6eae0ec230e8d0bd1f3
Tamas Vince <nemvince@proton.me>
2026-01-18 18:10:20 +0000

    refactor(ui): update Toaster theme and enhance CookiePopup logic

--- apps/iris/src/components/util/cookie-popup.tsx
+++ apps/iris/src/components/util/cookie-popup.tsx
@@ -1,36 +1,45 @@
-import { Cookie } from 'lucide-react';
+import { useEffect, useMemo } from 'react';
 import { useCookies } from 'react-cookie';
 import { useTranslation } from 'react-i18next';
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import { Button } from '@/components/ui/button';
+import { toast, useSonner } from 'sonner';
 
 export function CookiePopup() {
   const [cookies, setCookie] = useCookies(['filc.cookie-consent']);
+  const { toasts } = useSonner();
   const { t } = useTranslation();
 
-  const acceptCookies = () => {
-    setCookie('filc.cookie-consent', 'accepted', {
-      maxAge: 31_536_000,
-    });
-  };
-
-  if (cookies['filc.cookie-consent'] === 'accepted') {
-    return null;
-  }
-
-  return (
-    <Alert
-      className="fixed bottom-4 left-1/2 z-50 w-[90%] max-w-md -translate-x-1/2 md:w-full"
-      variant="default"
-    >
-      <Cookie />
-      <AlertTitle>{t('cookiePopup.title')}</AlertTitle>
-      <AlertDescription>
-        {t('cookiePopup.description')}
-        <Button className="cursor-pointer" onClick={acceptCookies}>
-          {t('common.accept')}
-        </Button>
-      </AlertDescription>
-    </Alert>
+  const isAccepted = useMemo(
+    () => cookies['filc.cookie-consent'] === 'accepted',
+    [cookies]
   );
+
+  const isToastActive = useMemo(
+    () => toasts.some((ts) => ts.id === 'cookie-consent'),
+    [toasts]
+  );
+
+  useEffect(() => {
+    if (!(isAccepted || isToastActive)) {
+      toast.info(t('cookiePopup.title'), {
+        action: {
+          label: t('common.accept'),
+          onClick: () => {
+            setCookie('filc.cookie-consent', 'accepted', {
+              maxAge: 31_536_000,
+            });
+          },
+        },
+        description: t('cookiePopup.description'),
+        id: 'cookie-consent',
+      });
+    }
+  }, [isAccepted, t, setCookie, isToastActive]);
+
+  useEffect(() => {
+    if (isAccepted && isToastActive) {
+      toast.dismiss('cookie-consent');
+    }
+  }, [isAccepted, isToastActive]);
+
+  return null;
 }
